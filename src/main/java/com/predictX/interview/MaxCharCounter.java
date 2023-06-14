dd93d2a5a1b4c4be8de1520a2a25b6e58e1010a2
R S Shiva Krishna M <rsshivakrishnam@Rs-MacBook-Pro.local>
2023-06-14 20:12:49 +0100

    Map and TreeSet

--- src/main/java/com/predictX/interview/MaxCharCounter.java
+++ src/main/java/com/predictX/interview/MaxCharCounter.java
@@ -7,31 +7,6 @@ import java.util.stream.Collectors;
 
 @Component
 public class MaxCharCounter {
-
-    // take count of only lowercase characters
-    // if count is 1 of character ignore it
-    // s1 = "A aaaa bb c"
-    // s2 = "& aaa bbb c d"
-    // res = "1:aaaa/2:bbb"
-
-    //   "=:"
-    // res should be in decreasing order of their length
-    // alphabetically; the different groups will be separated by '/'.
-
-    /*
-    s1 = "my&friend&Paul has heavy hats! &"
-    s2 = "my friend John has many many friends &"
-    mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
-
-    s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
-    s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
-    mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
-
-    s1="Are the kids at home? aaaaa fffff"
-    s2="Yes they are here! aaaaa fffff"
-    mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
-
-     */
     public String mix(String s1, String s2) {
 
         Map<Character, Integer> str1CharToCountMap = charCountFunc(s1).entrySet().stream().filter(entry -> entry.getValue() > 1)
