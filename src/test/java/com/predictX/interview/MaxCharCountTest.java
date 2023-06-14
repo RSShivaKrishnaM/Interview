d35b860477db36f61bc09663346714822860770c
R S Shiva Krishna M <rsshivakrishnam@Rs-MacBook-Pro.local>
2023-06-14 21:05:00 +0100

    corrected
    Signed-off-by: rsshivakrishnam <rsshivakrishnam@gmail.com>

--- src/test/java/com/predictX/interview/MaxCharCountTest.java
+++ src/test/java/com/predictX/interview/MaxCharCountTest.java
@@ -9,30 +9,28 @@ public class MaxCharCountTest {
     @Test
     public void test1() {
         MaxCharCounter maxCharCounter = new MaxCharCounter();
-        String character = maxCharCounter.mix("A aaaa bb c", "& aaa bbb c d");
-        assertEquals("1:aaaa/2:bbb", character);
+        String resultantString = maxCharCounter.mix("A aaaa bb c", "& aaa bbb c d");
+        assertEquals("1:aaaa/2:bbb", resultantString);
     }
 
     @Test
     public void test2() {
         MaxCharCounter maxCharCounter = new MaxCharCounter();
-        String charcter = maxCharCounter.mix("my&friend&Paul has heavy hats! &", "my friend John has many many friends &");
-        assertEquals("2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss", charcter);
+        String resultantString = maxCharCounter.mix("my&friend&Paul has heavy hats! &", "my friend John has many many friends &");
+        assertEquals("2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss", resultantString);
     }
 
     @Test
     public void test3() {
         MaxCharCounter maxCharCounter = new MaxCharCounter();
-        System.out.println(maxCharCounter.mix("mmmmm m nnnnn y&friend&Paul has heavy hats! &", "my frie n d Joh n has ma n y ma n y frie n ds n&"));
-        String character = maxCharCounter.mix("mmmmm m nnnnn y&friend&Paul has heavy hats! &", "my frie n d Joh n has ma n y ma n y frie n ds n&");
-        assertEquals("1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss", character);
-
+        String resultantString = maxCharCounter.mix("mmmmm m nnnnn y&friend&Paul has heavy hats! &", "my frie n d Joh n has ma n y ma n y frie n ds n&");
+        assertEquals("1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss", resultantString);
     }
 
     @Test
     public void test4() {
         MaxCharCounter maxCharCounter = new MaxCharCounter();
-        String character = maxCharCounter.mix("Are the kids at home? aaaaa fffff", "Yes they are here! aaaaa fffff");
-        assertEquals("=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh", character);
+        String resultantString = maxCharCounter.mix("Are the kids at home? aaaaa fffff", "Yes they are here! aaaaa fffff");
+        assertEquals("=:aaaaaa/2:eeeee/=:fffff/2:rr/1:tt/=:hh", resultantString);
     }
 }
